201ee64fbe34fee7370838defb11a012ceb4073e
Angel Motta <angelmotta@gmail.com>
2023-05-17 18:34:25 -0500

    feat: indexed triangle sample

--- hello_triangle_indexed.cpp
+++ hello_triangle_indexed.cpp
@@ -1,4 +1,4 @@
-#include "glad/glad.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -26,6 +26,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
 int main()
 {
     // glfw: initialize and configure
+    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -36,6 +37,7 @@ int main()
 #endif
 
     // glfw window creation
+    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -44,10 +46,10 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-    // We do have to tell GLFW we want to call this function on every window resize by registering it
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: load all OpenGL function pointers
+    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -56,6 +58,7 @@ int main()
 
 
     // build and compile our shader program
+    // ------------------------------------
     // vertex shader
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -95,74 +98,49 @@ int main()
     glDeleteShader(fragmentShader);
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
-    /*
-    float vertices[] = {
-            -0.5f, -0.5f, 0.0f, // left
-            0.5f, -0.5f, 0.0f, // right
-            0.0f,  0.5f, 0.f,  // top
-            1,-1,0,
-            1,1,0,
-            0.5,1, 0
-    };
-     */
-    // Mi piramide
+    // ------------------------------------------------------------------
 //    float vertices[] = {
-//            // Front face (visible)
-//            0.0f, 0.5f, 0.0f,     // Top vertex
-//            -0.5f, -0.5f, 0.5f,   // Bottom left vertex
-//            0.5f, -0.5f, 0.5f,    // Bottom right vertex
-//
-//            // Right face (default no visible)
-//            0.0f, 0.5f, 0.0f,     // Top vertex
-//            0.5f, -0.5f, 0.5f,    // Bottom left vertex
-//            0.5f, -0.5f, -0.5f,   // Bottom right vertex (reducir 'x' para verlo)
-//
-//            // Back face
-//            0.0f, 0.5f, 0.0f,     // Top vertex
-//            0.5f, -0.5f, -0.5f,   // Bottom left vertex
-//            -0.5f, -0.5f, -0.5f,  // Bottom right vertex
-//
-//            // Left face (default no visible)
-//            0.0f, 0.5f, 0.0f,     // Top vertex
-//            -0.5f, -0.5f, -0.5f,  // Bottom left vertex
-//            -0.5f, -0.5f, 0.5f,   // Bottom right vertex
-//
-//            // Bottom face (default no visible)
-//            -0.5f, -0.5f, 0.5f,   // Bottom left vertex
-//            0.5f, -0.5f, 0.5f,    // Bottom right vertex
-//            0.5f, -0.5f, -0.5f,   // Top vertex
-//            -0.5f, -0.5f, -0.5f   // Top vertex     // Bottom right vertex
+//            0.5f,  0.5f, 0.0f,  // top right
+//            0.5f, -0.5f, 0.0f,  // bottom right
+//            -0.5f, -0.5f, 0.0f,  // bottom left
+//            -0.5f,  0.5f, 0.0f   // top left
 //    };
+
+    // Mi piramide
     float vertices[] = {
-            0, 0, 0,
-            0.25, 0, 0.5,
-            0.5, 0, 0, // BASE
-            0.25, 0, 0.5,
-            0.5, 0, 0,
-            0.25, 0.5, 0, //cara derecha
-            0, 0, 0,
-            0.25, 0, 0.5,
-            0.25, 0.5, 0, // cara izquierda
-            0, 0, 0,
-            0.5, 0, 0,
-            0.25, 0.5, 0 //frontal
+            0, 0, 0,       // IZQUIERDO
+            0.7, 0.2, 0.5,  // PROFUNDO
+            0.5, 0, 0,     // DERECHO
+            0.25, 0.5, 0, // ARRIBA
     };
 
-
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 2,  // 1 Triangle
+            1, 2, 3,   // 2 Triangle
+            2, 3, 0,   // 3 Triangle
+            3, 0, 1   // 4 Triangle
+    };
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
@@ -173,37 +151,46 @@ int main()
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
+    // -----------
     while (!glfwWindowShouldClose(window))
     {
         // input
+        // -----
         processInput(window);
 
         // render
+        // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 12);
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -211,9 +198,12 @@ void processInput(GLFWwindow *window)
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
-}
+}//
+// Created by Angel Motta on 17/05/23.
+//
